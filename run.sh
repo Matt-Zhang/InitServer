082773c43d0c5247958a944a345054e910ad5f02
matt-zhang <mattzhang9@gmail.com>
2017-02-05 17:31:04 -0500

    docker and python

--- run.sh
+++ run.sh
@@ -6,8 +6,21 @@ apt-get update
 echo "Install ZSH..."
 cd zsh
 bash -x run.sh
+cd ..
 
 #vim
 cp .vimrc ~/.vimrc
 
+# docker
+# set to stable repo
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+apt-get install software-properties-common
+add-apt-repository "deb https://apt.dockerproject.org/repo/ubuntu-$(lsb_release -cs) main"
+# replace docker-engine with docker.io because the former one does not exists
+apt-get install docker.io
+docker run hello-world
 
+# python
+apt-get install python-setuptools python-dev build-essential 
+easy_install pip 
+pip install --upgrade virtualenv 
